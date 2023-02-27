aa2225d09545fbc59e13acbe8c0e4eb9539a1464
Maria Ghafar <Maria.Ghafar@fff>
2023-02-27 17:38:42 +0500

    javacript

---
+++ Admin1/js/orders.js
@@ -0,0 +1,27 @@
+const Orders =[
+    {
+        productName:'Folder mini Drone',
+        productNumber:'85631',
+        productStatus:'Due',
+        shipping:'pending'
+    },
+    {
+        productName:'Larvender Kf102 Drone',
+        productNumber:'36378',
+        productStatus:'Refunded',
+        shipping:'Declined'
+
+    },
+    {
+        productName:'Larvender Kf102 Drone',
+        productNumber:'36378',
+        productStatus:'Due',
+        shipping:'pending'
+
+    },{
+        productName:'Larvender Kf102 Drone',
+        productNumber:'36378',
+        productStatus:'Paid',
+        shipping:'Deliverd'
+    }
+]
